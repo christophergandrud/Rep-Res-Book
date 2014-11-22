48c612447ced098bb4d8985cb5ca1c26900f7e51
christophergandrud <christopher.gandrud@gmail.com>
2014-11-22 13:52:43 +0100

    removed plyr rename

--- BookMake.R
+++ BookMake.R
@@ -1,24 +1,25 @@
 #################
 # Make file for the book Reproducible Research with R and RStudio
 # Christopher Gandrud
-# Updated: 4 September 2014
+# Updated: 22 November 2014
 #################
 
 # This R source code compiles the manuscript for the book Reproducible Research with R and RStudio.
 # It also updates the main README file.
 
 ## Installing required packages
-# Note: To install the R packages used to compile the book open the Source/Children/FrontMatter/Packages.Rnw. 
-# Find: doInstall <- FALSE in the code chunk labeled "FrontPackageCitations". 
+# Note: To install the R packages used to compile the book open the Source/Children/FrontMatter/Packages.Rnw.
+# Find: doInstall <- FALSE in the code chunk labeled "FrontPackageCitations".
 # Change the value `FALSE` to `TRUE` and run the code chunk.
 
 # Load knitr package
 library(knitr)
 
-# Specify working directories. Change as needed.
-## Rep-Res-Parent.Rnw
+#### Specify working directories. Change as needed. ####
+# Rep-Res-Parent.Rnw
 ParentDirectory <- "/git_repositories/Rep-Res-Book/Source/"
-## README.Rmd
+
+# README.Rmd
 SetupDirectory <- "/git_repositories/Rep-Res-Book/"
 
 ##### Create PDF Book Manuscript ####
@@ -32,7 +33,5 @@ knitr::knit2pdf(input = "Rep-Res-Parent.Rnw")
 # extrafont::embed_fonts("Rep-Res-Parent.pdf")
 
 #### README ####
-# Change working directory to /Rep-Res-Book/Writing_Setup/
 setwd(SetupDirectory)
-
 knitr::knit(input = "README.Rmd", output = "README.md")
